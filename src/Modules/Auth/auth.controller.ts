36f374ef266cf2960e9338c7d6116b3f5ea76cbe
Yura <yura5742248@gmail.com>
2023-09-18 21:27:13 +0300

    delete passport strategy

--- src/Modules/Auth/auth.controller.ts
+++ src/Modules/Auth/auth.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from '../../Entity/dto/registerDto';
-import { LocalAuthenticationGuard } from './auth.guard';
 
 
 @Controller('auth')
@@ -10,16 +9,8 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
-   registerUser (@Body() registerDto: RegisterDto ){
-  return this.authService.register(registerDto)
+  registerUser(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 
-  @HttpCode(200)
-  @UseGuards(LocalAuthenticationGuard)
-  @Post('log-in')
-  async logIn(@Req() request: any) {
-    const user = request.user;
-    user.password = undefined;
-    return user;
-  }
 }
--- src/Modules/Auth/auth.module.ts
+++ src/Modules/Auth/auth.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../User/user.module';
-import { User } from '../../Entity/user.entity';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
 
 
 @Module({
   controllers:[AuthController],
-  providers: [AuthService, LocalStrategy],
-  imports:[UserModule, PassportModule],
+  providers: [AuthService],
+  imports:[UserModule],
   exports:[AuthService]
 })
 

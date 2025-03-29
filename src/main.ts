bffe33288ea558b695039fb387eafc5dcbd150a4
Yura <yura5742248@gmail.com>
2025-03-29 15:55:06 +0300

    get sales with filter

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "https://coplasca.vercel.app/",
     credentials: true
   });
 
